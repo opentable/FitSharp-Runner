40b8df451b4c48327768630b3fbfc31dee02f39e
Tony Sun <tony.sun@opentable.com>
2019-11-22 16:06:17 -0800

    checkin FitSharp-Runner

---
+++ Runner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using fitnesse.fitserver;
+using fitSharp.IO;
+using fitSharp.Machine.Engine;
+
+namespace Runner
+{
+	public class Program
+	{
+		static int Main(string[] arguments)
+		{
+			FitServer fitServer = new FitServer();
+
+			if (null != arguments && null != arguments[0] && !RuntimeInformation.IsOSPlatform(OSPlatform.Windows ))
+			{
+				arguments[0] = arguments[0].Replace(":", ";");
+			}
+
+			fitServer.Run(arguments, new TypeDictionary(), new ConsoleReporter());
+
+			return 0;
+		}
+	}
+}
